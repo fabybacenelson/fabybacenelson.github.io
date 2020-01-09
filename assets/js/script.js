a91b8483c62352db7a1b46060794ef9cfcd36d12
Nellie <fabybacenelson@gmail.com>
2020-01-08 18:05:03 -0700

    desktop projects page + screenshots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
 
-    $("#codeQuiz").hover(function(){
-        $("#screenHex").style()
-    },function(){
+    if(Math.max(document.documentElement.clientWidth) >= 800){
+        $("#codeQuiz").hover(function(){
+            $("#screenHex").css("display","block");
+            $("#screenLink").attr("xlink:href","./assets/images/quizScreen.png");
+        },function(){
+            $("#screenHex").css("display","none");
+        });
 
-    });
+        $("#passGen").hover(function(){
+            $("#screenHex").css("display","block");
+            $("#screenLink").attr("xlink:href","./assets/images/passScreen.png");
+        },function(){
+            $("#screenHex").css("display","none");
+        });
 
+        $("#weathDash").hover(function(){
+            $("#screenHex").css("display","block");
+            $("#screenLink").attr("xlink:href","./assets/images/weathScreen.png");
+        },function(){
+            $("#screenHex").css("display","none");
+        });
+    }
 });
