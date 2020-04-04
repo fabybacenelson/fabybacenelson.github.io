323f199a16dfbaf42089d87c1c3804aae364f0ad
Nellie <fabybacenelson@gmail.com>
2020-04-03 21:06:51 -0700

    started webpack and react config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+let path = require("path");
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        } 
+      ]
+    },
+    entry: "./src/index.jsx",
+    output: {
+        path: path.resolve("./dist/js"),
+        filename: "script.bundle.js"
+    },
+    resolve: {extensions: ['.js', '.jsx']}
+  };
